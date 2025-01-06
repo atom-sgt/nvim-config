f7d2a6950b8a50c8d2318b34ed8ec917a42cc41c
Adam Sargent <adamcsargent@gmail.com>
2025-01-05 19:36:30 -0500

    Modifications to plugins and colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,13 +1,19 @@
-vim.g.colortemplate_toolbar = false
+vim.cmd.colorscheme("everforest")
 
+vim.g.colortemplate_toolbar = false
 return {
-  'lifepillar/vim-colortemplate',
-  'tjdevries/colorbuddy.nvim',
-  'lifepillar/vim-gruvbox8',
-  'morhetz/gruvbox',
-  'sjl/badwolf',
-  'joshdick/onedark.vim',
-  'nordtheme/vim',
-  'sainnhe/everforest',
-  'junegunn/seoul256.vim',
+	-- Tools
+	"lifepillar/vim-colortemplate",
+	"tjdevries/colorbuddy.nvim",
+	-- Colors
+	"morhetz/gruvbox",
+	"lifepillar/vim-gruvbox8",
+	"luisiacc/gruvbox-baby",
+	"sjl/badwolf",
+	"joshdick/onedark.vim",
+	"nordtheme/vim",
+	"sainnhe/everforest",
+	"folke/tokyonight.nvim",
+	"junegunn/seoul256.vim",
+	"slugbyte/lackluster.nvim",
 }
--- lua/plugins/netrw.lua
+++ lua/plugins/netrw.lua
@@ -1,10 +1,11 @@
-vim.keymap.set('n', '<leader>nn', vim.cmd.Lexplore)
+vim.keymap.set("n", "<leader>nn", vim.cmd.Explore)
 
 vim.g.netrw_banner = 0 -- No banner
 vim.g.netrw_liststyle = 3 -- Tree style
 -- vim.g.netrw_browse_split = 4 -- Open in
 -- vim.g.netrw_altv = 1 -- v split to right instead of left
 -- vim.g.netrw_winsize = -25
+vim.g.autochdir = true -- auto change working dir after file open
 
 -- auto open on startup
 -- vim.api.nvim_create_augroup("ProjectDrawer", { clear = true })
@@ -13,6 +14,5 @@ vim.g.netrw_liststyle = 3 -- Tree style
 --   pattern = "*",
 --   command = "Vexplore"
 -- })
--- vim.g.autochdir = true -- auto change working dir after file open
 
 return {}
